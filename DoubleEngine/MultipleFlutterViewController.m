b455f69c8bc360c44ac5f8a9c7aba6b7ca12074e
Aaron Clarke <aaclarke@google.com>
2020-10-14 13:21:51 -0700

    added the ability to specify to use spawn

--- DoubleEngine/MultipleFlutterViewController.m
+++ DoubleEngine/MultipleFlutterViewController.m
@@ -8,6 +8,8 @@
 #import "MultipleFlutterViewController.h"
 #import <Flutter/Flutter.h>
 
+#define USE_SPAWN
+
 @interface MultipleFlutterViewController ()
 
 @end
@@ -25,8 +27,22 @@
 - (void)viewDidAppear:(BOOL)animated {
   [super viewDidAppear:animated];
   const CGFloat height = UIScreen.mainScreen.bounds.size.height / _numberOfFlutters;
+  FlutterEngine* mainEngine = nil;
   for (int i = 0; i < _numberOfFlutters; ++i) {
-    FlutterViewController* vc = [[FlutterViewController alloc] init];
+    FlutterEngine* engine = nil;
+    if (!mainEngine) {
+      mainEngine = [[FlutterEngine alloc] initWithName:@"main"];
+      [mainEngine run];
+      engine = mainEngine;
+    } else {
+#ifdef USE_SPAWN
+      engine = [mainEngine spawnWithEntrypoint:nil];
+#else
+      engine = [[FlutterEngine alloc] initWithName:@"secondary"];
+      [engine run];
+#endif
+    }
+    FlutterViewController* vc = [[FlutterViewController alloc] initWithEngine:engine nibName:nil bundle:nil];
     [self addChildViewController:vc];
     [self.view addSubview:vc.view];
     vc.view.frame = CGRectMake(0, i * height, UIScreen.mainScreen.bounds.size.width, height);
